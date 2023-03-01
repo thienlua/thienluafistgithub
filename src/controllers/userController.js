64763111908f81e4436b8e8c33b32de9db10ac69
Nguyen Nam <nguyendiennam191095@gmail.com>
2023-03-01 20:14:33 +0700

    secondcommit

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,24 +1,25 @@
 import userService from "../services/userService";
-let handleLogin = async(req,res) => {
-    let email = req.body.email;
+
+let handleLogin = async(req,res) =>{
+    let email =req.body.email;
     let password = req.body.password;
-    if (!email||!password) {
-    return res.status(500).json ({
-        errCode: 1,
-        message: 'Missing inputs parameter!'
-    })
+    if(!email||!password) {
+        return res.status(500).json({
+            errCode: 1,
+            message: 'Missing inputs paramter'
+        })
     }
     let userData = await userService.handleUserLogin(email, password);
     //check email exist
     //compare password
     //return userInfor
-    //access_token: JWT
+    //access token: JWT json web token
     return res.status(200).json({
         errCode: userData.errCode,
         message: userData.errMessage,
-        userData
+        user: userData.user ? userData.user : {}
     })
 }
 module.exports = {
-    handleLogin: handleLogin
+    handleLogin:handleLogin
 }
--- src/route/web.js
+++ src/route/web.js
@@ -13,7 +13,7 @@ let initWebRoutes =(app) => {
     router.get('/edit-crud', homeController.getEditCRUD);
     router.post('/put-crud', homeController.putCRUD);
     router.get('/delete-crud', homeController.deleteCRUD);
-    router.post('/api/login', userController.handleLogin);
+    router.post('/api/login',userController.handleLogin);
         return app.use("/",router);
 }       
 
